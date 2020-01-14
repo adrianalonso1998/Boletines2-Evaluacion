adc26ad8f048f76020dfac34e45955f2d77a039a
JAIMEGSDAM <jaimegsdam@gmail.com>
2020-01-14 12:37:24 +0100

    .

---
+++ 2EvBoletin2/src/clasesyobjetos/Persona.java
@@ -0,0 +1,30 @@
+package clasesyobjetos;
+import java.util.Scanner;
+public class Persona {
+	Scanner sc = new Scanner(System.in);
+	
+	public String nombre;
+	public String apellidos;
+	public String fechaNac;
+	public String estatura;
+
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		Persona p1= new Persona();
+		p1.nombre=sc.nextLine();
+		p1.apellidos=sc.nextLine();
+		p1.fechaNac=sc.nextLine();
+		p1.estatura=sc.nextLine();
+		
+		java.util.Date fechaActual = new java.util.Date();
+		System.out.println(fechaActual);
+	}
+
+
+
+	
+
+}
